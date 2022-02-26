3b44e07304c32fd914a19d90934ff5ed3090f5b6
Kevin Newman <kevin@unfocus.com>
2022-02-26 14:55:21 -0500

    feat: Add some recovery sync features for hass restart
    - Also fixes config. Getting the color_modes to register is
    frustrating. The docs are not clear about which props need
    to be set relative to the others to get home assistant to
    accept the settings. Currently, I've had to remove
    color_temp and rgb to get it to work.

--- index.js
+++ index.js
@@ -16,11 +16,11 @@ function publish(topic, message) {
   });
 }
 
-async function set_on(id, state) {
-  const path = `/api/device/set_on?id=${id}&on=${state === "ON" ? "1" : "0"}`;
+async function set_on(id, payload) {
+  const path = `/api/device/set_on?id=${id}&on=${payload.state === "ON" ? "1" : "0"}`;
 
   // Optimistically set the result through MQTT
-  publish(`homeassistant/gecync/state/${id}`, state);
+  publish(`homeassistant/gecync/state/${id}`, JSON.stringify(payload));
 
   const result = await fetch(CBYGE_URL + path);
   const status = await result.json();
@@ -38,7 +38,7 @@ async function set_brightness(id, brightness) {
   const path = `/api/device/set_brightness?id=${id}&brightness=${brightness}`;
 
   // Optimistically set the result through MQTT
-  publish(`homeassistant/gecync/brightness/${id}`, brightness);
+  publish(`homeassistant/gecync/brightness/${id}`, JSON.stringify(brightness));
 
   const result = await fetch(CBYGE_URL + path);
   const status = await result.json();
@@ -57,10 +57,11 @@ async function configure() {
   console.log("fetching devices...");
   const result = await fetch(CBYGE_URL + "/api/devices?update_status=1");
   const devices = await result.json();
-  console.log(devices);
+  // console.log(devices);
 
   // Subscribe to status changes from home assistant
   mqtt_client.subscribe("homeassistant/gecync/#");
+  mqtt_client.subscribe("homeassistant/status");
 
   // Send config to home assistant through mqtt
   devices.forEach(function (light) {
@@ -75,10 +76,12 @@ async function configure() {
       brightness_state_topic: "homeassistant/gecync/brightness/" + light.id,
       brightness_command_topic:
         "homeassistant/gecync/set-brightness/" + light.id,
+      color_temp_state_topic: "homeassistant/gecync/color-temp/" + light.id,
+      color_temp_command_topic: "homeassistant/gecync/set-color-temp/" + light.id,
+      brightness: true,
       brightness_scale: 100,
-      payload_on: "ON",
-      payload_off: "OFF",
-      supported_color_modes: ["onoff", "brightness", "color_temp", "rgb"],
+      color_mode: true,
+      supported_color_modes: ["brightness"],
       device: {
         identifiers: [light.id, light.name],
         name: light.name,
@@ -88,40 +91,60 @@ async function configure() {
       },
     };
 
-    mqtt_client.publish(topic, JSON.stringify(payload), {
-      qos: 1,
-      retain: true,
-    });
+    publish(topic, JSON.stringify(payload));
 
     // Set the status through MQTT
-    mqtt_client.publish(
+    publish(
       `homeassistant/gecync/state/${light.id}`,
-      light.status.is_on ? "ON" : "OFF",
-      {
-        qos: 1,
-        retain: true,
-      }
+      JSON.stringify({ state: light.status.is_on ? "ON" : "OFF" })
     );
   });
 }
 
 /* subscribe to some topics */
-mqtt_client.on("connect", async function () {
+mqtt_client.on("connect", function () {
   console.log("connected");
   configure();
 });
 
+mqtt_client.on("reconnect", function () {
+  console.log("reconnected");
+  configure();
+});
+
+function retryConnection() {
+  mqtt_client.reconnect();
+}
+
+mqtt_client.on("close", function () {
+  console.log("connection lost");
+  // wait a while then reconnect
+  setTimeout(retryConnection, 15000);
+});
+
+mqtt_client.on("disconnect", function () {
+  console.log("disconnected");
+  // wait a while then reconnect
+  setTimeout(retryConnection, 15000);
+});
+
 /* Forward MQTT packets to REST */
 mqtt_client.on("message", function (topic, message) {
-  console.log(topic.toString(), message.toString());
-  const [, , command, id] = topic.toString().split("/");
-  const payload = message.toString();
-  switch (command) {
-    case "set-state":
-      set_on(id, payload);
-      break;
-    case "set-brightness":
-      set_brightness(id, payload);
-      break;
+  console.log("message:", topic.toString(), message.toString());
+  const [, scope, command, id] = topic.toString().split("/");
+  const payload = JSON.parse(message.toString());
+  if (scope === "gecync") {
+    switch (command) {
+      case "set-state":
+        set_on(id, payload);
+        break;
+      case "set-brightness":
+        set_brightness(id, payload);
+        break;
+    }
+  } else if (scope === "status") {
+    if (payload === "online") {
+      configure();
+    }
   }
 });
